56ebcde231d685e51b1ce4b74f740b258cc6ab29
nishanthip25 <nishakavitha1234@gmail.com>
2026-06-12 15:33:26 +0530

    functions.js

---
+++ functions.js
@@ -0,0 +1,53 @@
+console.log("Welcome to Primitive Datatypes");
+
+// Number
+console.log("DT Number");
+var age = 30;
+console.log(age);
+
+// String
+console.log("DT String");
+var name = "Nisha";
+console.log(name);
+
+// Boolean
+console.log("DT Boolean");
+var isStudent = true;
+console.log(isStudent);
+
+// Null
+console.log("DT Null");
+var emptyValue = null;
+console.log(emptyValue);
+
+// Undefined
+console.log("DT Undefined");
+var undefinedValue;
+console.log(undefinedValue);
+
+// Symbol
+console.log("DT Symbol");
+var symbolValue = Symbol("symbol");
+console.log(symbolValue);
+
+// BigInt
+console.log("DT BigInt");
+var bigIntValue = 1211234567890234567890n;
+console.log(bigIntValue);
+
+console.log("WELCOME TO NON-PRIMITIVE DATATYPES");
+
+// Object
+console.log("DT Object");
+var person = {
+    name: "Nisha",
+    age: 18
+};
+console.log(person);
+console.log(person.age);
+
+// Array
+console.log("DT Array");
+var fruits = ["apple", "orange", "pine", "fig", "grapes"];
+console.log(fruits.length);
+console.log(fruits[4]);
