21cd6c57ae88f92b528ee12997ccae0af00ad108
nishanthip25 <nishakavitha1234@gmail.com>
2026-06-12 11:36:24 +0530

    operators.js

--- operators.js
+++ operators.js
@@ -8,6 +8,14 @@ console.log(a/b);//division
 console.log(a%b);//modulus
 console.log(a**b);//exponentiation
 
+//assignment
+console.log ("assignment operators")
+var c =30;
+console.log(c+=10)
+console.log(c-=10)
+console.log(c*=10)
+console.log(c/=10)
+
 
 
 
