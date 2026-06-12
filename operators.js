b7dea574f1e17bdb491dcfc4c70996a4c4ace471
nishanthip25 <nishakavitha1234@gmail.com>
2026-06-12 11:18:31 +0530

    operators

---
+++ operators.js
@@ -0,0 +1,14 @@
+//arthimetic
+var a =10;
+var b =20;
+console.log(a+b);//addition
+console.log(a-b);//subraction
+console.log(a*b);//multiplication
+console.log(a/b);//division
+console.log(a%b);//modulus
+console.log(a**b);//exponentiation
+
+
+
+
+
