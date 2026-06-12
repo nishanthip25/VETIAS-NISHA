4712631e9f48b63839d556377e7a5e7217da5566
nishanthip25 <nishakavitha1234@gmail.com>
2026-06-12 10:25:43 +0530

    variable example

---
+++ variable.js
@@ -0,0 +1,13 @@
+//variable declaration using var
+var name ="vellalar"
+console.log(name)
+
+//variable declaration using let
+{
+    let age =25
+    console.log(age)
+}
+
+//variable declaration using const
+const country ="india"
+console.log(country)
